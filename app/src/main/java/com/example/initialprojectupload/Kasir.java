0c75648369471e6d51b1197beff6574adaca6387
nblsptr73 <nblsptr73@gmail.com>
2026-06-07 01:09:41 +0700

    ggg

--- app/src/main/java/com/example/initialprojectupload/Kasir.java
+++ app/src/main/java/com/example/initialprojectupload/Kasir.java
@@ -1,24 +1,84 @@
 package com.example.initialprojectupload;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import android.content.Intent;
+import android.widget.Button;
 
 public class Kasir extends AppCompatActivity {
+
+    ListView listProdukKasir;
+    DBHelper dbHelper;
+
+    ArrayList<String> dataProduk;
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_kasir);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        listProdukKasir = findViewById(R.id.listProdukKasir);
+
+        dbHelper = new DBHelper(this);
+
+        tampilkanProduk();
+        Button btnLogout;
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Kasir.this, Home.class);
+
+            startActivity(intent);
+
+            finish();
+
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tampilkanProduk();
+    }
+
+    private void tampilkanProduk() {
+
+        dataProduk = new ArrayList<>();
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(
+                "SELECT * FROM produk",
+                null
+        );
+
+        while(cursor.moveToNext()) {
+
+            dataProduk.add(
+                    cursor.getString(1)
+                            + " - Rp "
+                            + cursor.getInt(2)
+            );
+        }
+
+        adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dataProduk
+        );
+
+        listProdukKasir.setAdapter(adapter);
+
+        cursor.close();
     }
 }
